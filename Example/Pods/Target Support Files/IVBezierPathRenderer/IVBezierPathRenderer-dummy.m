b4a113c570a9164b67acf714c0deee6a920667a9
Ivan <ivan114@me.com>
2016-06-16 21:16:51 +0800

    Init

---
+++ Example/Pods/Target Support Files/IVBezierPathRenderer/IVBezierPathRenderer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_IVBezierPathRenderer : NSObject
+@end
+@implementation PodsDummy_IVBezierPathRenderer
+@end
---
+++ Example/Pods/Target Support Files/IVBezierPathRenderer/IVBezierPathRenderer-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double IVBezierPathRendererVersionNumber;
+FOUNDATION_EXPORT const unsigned char IVBezierPathRendererVersionString[];
+
