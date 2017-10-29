1763d9a91b526b4f984e9a89fdcf2068dd04ce9b
Ivan <ivan114@me.com>
2017-10-29 13:52:19 +0800

    ignore Example/Pods

--- Example/Pods/Target Support Files/IVBezierPathRenderer/IVBezierPathRenderer-umbrella.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "IVBezierPathRenderer.h"
-#import "UIBezierPath+GuidedPath.h"
-
-FOUNDATION_EXPORT double IVBezierPathRendererVersionNumber;
-FOUNDATION_EXPORT const unsigned char IVBezierPathRendererVersionString[];
-
